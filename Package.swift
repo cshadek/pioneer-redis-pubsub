b2fde0fd1000cfebe7eb044061d34f797135928c
Chris Shadek <cshadek@vegaconnected.com>
2023-10-21 16:48:58 -0500

    Trying again

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/cshadek/pioneer", branch: "public_no_arguments"),
-        .package(url: "https://gitlab.com/mordil/RediStack.git", from: "2.0.0-gamma.1"),
+        .package(url: "https://gitlab.com/mordil/RediStack.git", exact: "2.0.0-gamma.1.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
